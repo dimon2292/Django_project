1a74a88c0914664f531f794bcb989c1a0c2b7570
dimon2292 <dimmmmmma@mail.ru>
2023-06-16 21:11:08 +0300

    third commit

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import CartView, ProductView, ShopView
 
-
+app_name = 'store'
 
 urlpatterns = [
-    path('cart', CartView.as_view()),
-    path('product', ProductView.as_view()),
-    path('shop', ShopView.as_view()),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('product/<int:id>/', ProductView.as_view(), name='product'),
+    path('', ShopView.as_view(), name='shop')
 ]
